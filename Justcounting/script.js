475226e283c146edb0ec2fb6c8ab5b7ddbaf8bf3
Mahavishnu <mahavishnureddya@gmail.com>
2021-01-14 22:19:24 -0500

    Working

--- Justcounting/script.js
+++ Justcounting/script.js
@@ -1,13 +1,22 @@
-
-
 $(
     console.log("hey"),
     N=Date.now(),
     console.log(N),
+    document.getElementById("tryAgain").onclick=function(){
+        N=Date.now();
+        console.log("In try again")
+        console.log(N)  
+    },
     document.getElementById("stopP").onclick=function(){
         NT=Date.now();
-        console.log((NT-N)/1000)
-        
+        diffE=(NT-N)/1000;
+        if (diffE>60){
+            diffE=diffE/60;
+        }
+        console.log(diffE);
+        document.getElementById("resUlt").innerHTML = diffE;
+        if (diffE>60){
+            document.getElementById("resUlt").innerHTML = diffE+" min";
+        }
     }
-
 )
